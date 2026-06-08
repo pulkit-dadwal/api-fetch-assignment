2c5f54d8356f98eb6366ca3921774e4f3d039c71
Pulkit Dadwal <pulkitdadwal@Pulkits-Mac-mini.local>
2026-06-08 17:24:52 +0530

    modified main.jsx to use router functions and created navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="logo">
+        TVMaze Explorer
+      </Link>
+
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
